7f8ed7cb29f6dd1545ca043c2820b28799cb07b4
Kozloff <mkozloff@spartan.northampton.edu>
2018-05-02 18:52:18 -0400

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
         <style>
             html, body {
                 background-color: #fff;
+                
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
